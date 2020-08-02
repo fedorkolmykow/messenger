498b33bfb14b4f6f15617fa79abe62ea537861b6
fedorkolmykow <fedorkolmykow@gmail.ru>
2020-08-03 00:32:40 +0300

    Add sorting interface

--- message_service/pkg/modeles/Chat.go
+++ message_service/pkg/modeles/Chat.go
@@ -1,10 +1,12 @@
 package modeles
 
+import "time"
+
 type Chat struct {
-	ChatId    string  `json:"id"`
-	Name      string  `json:"name"`
-	Users     []User  `json:"users"`
-	CreatedAt string  `json:"created_at"`
+	ChatId    string       `json:"id"`
+	Name      string       `json:"name"`
+	Users     []User  	   `json:"users"`
+	CreatedAt time.Time    `json:"created_at"`
 }
 
 type ChatAddRequest struct {
@@ -24,3 +26,16 @@ type ChatsGetResponse struct {
 	Chats      []Chat  `json:"chats"`
 }
 
+func (s ChatsGetResponse) Less(i, j int) bool {
+	return s.Chats[i].CreatedAt.Before(s.Chats[j].CreatedAt)
+}
+
+func (s ChatsGetResponse) Swap(i, j int) {
+	s.Chats[i].CreatedAt,
+		s.Chats[j].CreatedAt = s.Chats[j].CreatedAt,
+		s.Chats[i].CreatedAt
+}
+
+func (s ChatsGetResponse) Len() int {
+	return len(s.Chats)
+}
--- message_service/pkg/modeles/Message.go
+++ message_service/pkg/modeles/Message.go
@@ -1,11 +1,13 @@
 package modeles
 
+import "time"
+
 type Message struct {
-	MessageId    string  `json:"id"`
-	ChatId       string  `json:"chat"`
-	AuthorId     string  `json:"author"`
-	Text         string  `json:"text"`
-	CreatedAt    string  `json:"created_at"`
+	MessageId    string     `json:"id"`
+	ChatId       string     `json:"chat"`
+	AuthorId     string     `json:"author"`
+	Text         string     `json:"text"`
+	CreatedAt    time.Time  `json:"created_at"`
 }
 
 type MessageAddRequest struct {
@@ -24,4 +26,18 @@ type MessagesGetRequest struct {
 
 type MessagesGetResponse struct {
 	Messages      []Message  `json:"messages"`
+}
+
+func (s MessagesGetResponse) Less(i, j int) bool {
+	return s.Messages[i].CreatedAt.Before(s.Messages[j].CreatedAt)
+}
+
+func (s MessagesGetResponse) Swap(i, j int) {
+	s.Messages[i].CreatedAt,
+	s.Messages[j].CreatedAt = s.Messages[j].CreatedAt,
+	s.Messages[i].CreatedAt
+}
+
+func (s MessagesGetResponse) Len() int {
+	return len(s.Messages)
 }
