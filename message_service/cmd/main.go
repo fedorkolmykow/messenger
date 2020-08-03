9d4c05cdd303fd466cbf3b46b3b57fba1e919d2d
fedorkolmykow <fedorkolmykow@gmail.ru>
2020-08-03 22:31:40 +0300

    main file ready

--- message_service/cmd/main.go
+++ message_service/cmd/main.go
@@ -1,3 +1,23 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net/http"
 
+	"avito_message/message_service/pkg/dbclient"
+	"avito_message/message_service/pkg/service"
+	"avito_message/message_service/pkg/service/httpserver"
+)
+
+func main() {
+	dbCon := dbclient.NewDb(context.Background())
+	service := service.NewService(dbCon)
+	server := httpserver.NewServer(service)
+
+	fmt.Println("starting server at :9000")
+	err := http.ListenAndServe(":9000", server)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
